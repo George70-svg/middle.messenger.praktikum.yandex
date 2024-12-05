db564c965d177765b7f83b69b47971199c2cf622
Anton <noname@example.com>
2024-12-05 21:41:13 +0500

    Add message page

--- src/app.ts
+++ src/app.ts
@@ -5,18 +5,27 @@ import { button } from './components/button/button.ts'
 import { input } from './components/input/input.ts'
 import { link } from './components/link/link.ts'
 import { footer } from './components/footer/footer.ts'
+import { chatListItem } from './components/chatLstItem/chatListItem.ts'
+import { ChatListItem, chatListMock } from './mock/chatList.ts'
 
 Handlebars.registerPartial('button', button)
 Handlebars.registerPartial('input', input)
 Handlebars.registerPartial('link', link)
 Handlebars.registerPartial('footer', footer)
+Handlebars.registerPartial('chatListItem', chatListItem)
 
 export default class App {
-  private state: { currentPage: PageType }
+  private state: {
+    currentPage: PageType,
+    chatList: ChatListItem[]
+  }
   private readonly appElement: HTMLElement | null
   
   constructor() {
-    this.state = { currentPage: 'loginPage' }
+    this.state = {
+      currentPage: 'loginPage',
+      chatList: chatListMock,
+    }
     this.appElement = document.getElementById('root')
   }
   
@@ -24,7 +33,9 @@ export default class App {
     if(this.state.currentPage && this.appElement) {
       const currentPage = this.state.currentPage
       const template = Handlebars.compile(Pages[currentPage])
-      this.appElement.innerHTML = template({})
+      this.appElement.innerHTML = template({
+        chatList: this.state.chatList,
+      })
     }
     
     this.attachEventListeners()
---
+++ src/mock/chatList.ts
@@ -0,0 +1,105 @@
+export type ChatListItem = {
+  name: string,
+  date: string,
+  lastMessage: string,
+  unreadMessageNumber: number,
+}
+
+export const chatListMock: ChatListItem[] = [
+  {
+    name: 'Антон',
+    date: '1 мая',
+    lastMessage: 'Непрочитанное сообщение',
+    unreadMessageNumber: 5,
+  },
+  {
+    name: 'Олег',
+    date: '2 мая',
+    lastMessage: 'Непрочитанное сообщение',
+    unreadMessageNumber: 5,
+  },
+  {
+    name: 'Антон',
+    date: '1 мая',
+    lastMessage: 'Непрочитанное сообщение',
+    unreadMessageNumber: 5,
+  },
+  {
+    name: 'Олег',
+    date: '2 мая',
+    lastMessage: 'Непрочитанное сообщение',
+    unreadMessageNumber: 5,
+  },
+  {
+    name: 'Антон',
+    date: '1 мая',
+    lastMessage: 'Непрочитанное сообщение',
+    unreadMessageNumber: 5,
+  },
+  {
+    name: 'Антон',
+    date: '1 мая',
+    lastMessage: 'Непрочитанное сообщение',
+    unreadMessageNumber: 5,
+  },
+  {
+    name: 'Антон',
+    date: '1 мая',
+    lastMessage: 'Непрочитанное сообщение',
+    unreadMessageNumber: 5,
+  },
+  {
+    name: 'Антон',
+    date: '1 мая',
+    lastMessage: 'Непрочитанное сообщение',
+    unreadMessageNumber: 5,
+  },
+  {
+    name: 'Антон',
+    date: '1 мая',
+    lastMessage: 'Непрочитанное сообщение',
+    unreadMessageNumber: 5,
+  },
+  {
+    name: 'Олег',
+    date: '2 мая',
+    lastMessage: 'Непрочитанное сообщение',
+    unreadMessageNumber: 5,
+  },
+  {
+    name: 'Антон',
+    date: '1 мая',
+    lastMessage: 'Непрочитанное сообщение',
+    unreadMessageNumber: 5,
+  },
+  {
+    name: 'Олег',
+    date: '2 мая',
+    lastMessage: 'Непрочитанное сообщение',
+    unreadMessageNumber: 5,
+  },
+  {
+    name: 'Антон',
+    date: '1 мая',
+    lastMessage: 'Непрочитанное сообщение',
+    unreadMessageNumber: 5,
+  },
+  {
+    name: 'Антон',
+    date: '1 мая',
+    lastMessage: 'Непрочитанное сообщение',
+    unreadMessageNumber: 5,
+  },
+  {
+    name: 'Антон',
+    date: '1 мая',
+    lastMessage: 'Непрочитанное сообщение',
+    unreadMessageNumber: 5,
+  },
+  {
+    name: 'Антон',
+    date: '1 мая',
+    lastMessage: 'Непрочитанное сообщение',
+    unreadMessageNumber: 5,
+  },
+]
