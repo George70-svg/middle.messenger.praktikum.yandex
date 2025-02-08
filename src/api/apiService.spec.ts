f6984be6baecafb63dbf4034def9810d3a59b927
Anton <noname@example.com>
2025-02-09 02:19:10 +0500

    Fix api test

--- src/api/apiService.spec.ts
+++ src/api/apiService.spec.ts
@@ -3,7 +3,6 @@ import { HTTPTransport, METHOD } from './apiService.ts'
 
 describe('HTTP Transport', () => {
   let http: HTTPTransport
-  let request: unknown
 
   beforeEach(() => {
     http = new HTTPTransport('BaseUrl')
@@ -13,15 +12,24 @@ describe('HTTP Transport', () => {
     vi.restoreAllMocks()
   })
 
-  it('should the request with the passed parameters must be executed', () => {
-    request = vi.spyOn(http, 'request')
-    http.get('/test-endpoint', { data: { a: 1, b: 2 } })
-    expect(request).toHaveBeenCalledWith('/test-endpoint', 'GET', expect.objectContaining({ data: { a: 1, b: 2 } }))
+  it('should the request with the passed parameters must be executed', async () => {
+    const requestSpy = vi.spyOn(http, 'request').mockImplementation(() => Promise.resolve())
+    await http.get('/test-endpoint', { data: { a: 1, b: 2 } })
+    expect(requestSpy).toHaveBeenCalledWith('/test-endpoint', 'GET', expect.objectContaining({ data: { a: 1, b: 2 } }))
   })
 
-  it('should data transform to string', () => {
-    request = vi.spyOn(XMLHttpRequest.prototype, 'open')
-    http.get('/test-endpoint', { data: { a: 1, b: 2 } })
-    expect(request).toHaveBeenCalledWith(METHOD.GET, expect.stringContaining('/test-endpoint?a=1&b=2'))
+  it('should data transform to string', async () => {
+    const openSpy = vi.spyOn(XMLHttpRequest.prototype, 'open').mockImplementation(() => {})
+
+    // Мокаю событие send, чтобы не отправить реальный запрос
+    vi.spyOn(XMLHttpRequest.prototype, 'send').mockImplementation(function (this: XMLHttpRequest) {
+      // Переопределяю readonly свойства
+      Object.defineProperty(this, 'status', { value: 200, configurable: true })
+      Object.defineProperty(this, 'response', { value: { ok: true }, configurable: true })
+      this.onload?.(new ProgressEvent('load'))
+    })
+
+    await http.get('/test-endpoint', { data: { a: 1, b: 2 } })
+    expect(openSpy).toHaveBeenCalledWith(METHOD.GET, expect.stringContaining('/test-endpoint?a=1&b=2'))
   })
 })
