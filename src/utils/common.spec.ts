44119f0f27755a8e5551738399723d1ea1041dea
Anton <noname@example.com>
2025-02-09 00:24:06 +0500

    Crash test

--- src/utils/common.spec.ts
+++ src/utils/common.spec.ts
@@ -7,6 +7,6 @@ describe('Utility queryStringify', () => {
   })
 
   it('should stringify data with deep array to query string', () => {
-    expect(queryStringify({ a: 1, b: [1, 'name', ['a', 'b', 'c']] })).to.equal('?a=1&b[0]=1&b[1]=name&b[2][0]=a&b[2][1]=b&b[2][2]=c')
+    expect(queryStringify({ a: 1, b: [1, 'name', ['a', 'b', 'c']] })).to.equal('?a=1&b[0]=1&b[1]=name&b[2][0]=a&b[2][1]=b&b[2][2]=c1')
   })
 })
