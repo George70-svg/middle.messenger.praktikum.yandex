6ccb8dae5dc1058bc4fb54e87918dc3f8f92b1f4
Anton <noname@example.com>
2025-02-09 00:47:58 +0500

    Crash teat

--- src/components/input/input.ts
+++ src/components/input/input.ts
@@ -56,7 +56,7 @@ export class Input extends Block {
     }
 
     const hintContainer = this.element?.querySelector('.hint-container') as HTMLElement
-    const value = this.getValue()?.trim()
+    let value = this.getValue()?.trim()
     if (!hintContainer) return
 
     if (value) {
